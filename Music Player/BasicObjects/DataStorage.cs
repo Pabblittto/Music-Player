9893f2d19f55a60309db31b1d50951f4f305c939
Pabblo <pawel.mat17@op.pl>
2020-01-12 17:15:28 +0100

    basics singletons

---
+++ Music Player/BasicObjects/DataStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Player.BasicObjects
+{
+    class DataStorage
+    {
+        private DataStorage instance;
+
+        private DataStorage()
+        {
+
+        }
+
+        public DataStorage getInstance()
+        {
+            if (instance == null)
+                instance = new DataStorage();
+            return instance;
+        }
+
+    }
+}
---
+++ Music Player/BasicObjects/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Music_Player.BasicObjects
+{
+    class Player
+    {
+        private Player instance;
+
+        private Player()
+        {
+
+        }
+
+        public Player getInstance()
+        {
+            if (instance == null)
+                instance = new Player();
+            return instance;
+        }
+
+    }
+}
