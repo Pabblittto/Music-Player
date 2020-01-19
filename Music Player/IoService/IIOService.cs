fe6a9d85b56aafc5541afb2e5f147e20c5e5fa61
Daniel Józef Niyski <szprytny14@gmail.com>
2020-01-19 18:53:29 +0100

    created IOService

---
+++ Music Player/IoService/IIOService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Music_Player.Models;
+
+namespace Music_Player.IoService
+{
+    interface IIOService
+    {
+        List<Song> SearchDirectory(string dir);
+        Playlist ReadPlaylist(string dir);
+        void SavePlaylist(Playlist playlist, string dir, SaveFormat format);
+    }
+
+    enum SaveFormat
+    {
+       XML = 1,
+       JSON = 2
+    }
+}
