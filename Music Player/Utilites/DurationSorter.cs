48aa0d25c4c0968eb26e99f181893a77cdb624af
Daniel Józef Niyski <szprytny14@gmail.com>
2020-01-20 21:36:19 +0100

    Jeb sortowanko na maxa

---
+++ Music Player/Utilites/DurationSorter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Music_Player.Models;
+
+namespace Music_Player.Utilites
+{
+    class DurationSorter : SorterDecorator
+    {
+        public DurationSorter(ISorter sorter) : base(sorter)
+        {
+        }
+
+        public override int SortFn(Song item1, Song item2)
+        {
+            return item1.duration.CompareTo(item2.duration);
+        }
+    }
+}
